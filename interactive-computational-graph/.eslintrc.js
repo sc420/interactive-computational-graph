b871d845e7273584decbab1132f2618906145de3
Shawn Chang <ebola777gm@gmail.com>
2023-07-14 22:54:12 +0800

    add ESLint config

---
+++ interactive-computational-graph/.eslintrc.js
@@ -0,0 +1,42 @@
+// ESLint configuration file
+//
+// Reference: https://eslint.org/docs/latest/use/configure/configuration-files
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "tsconfig.json"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    },
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    }
+}

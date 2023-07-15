4a19b542800e0e6a8347dc40fae57e4eb1fce64b
Shawn Chang <ebola777gm@gmail.com>
2023-07-15 16:49:24 +0800

    add Jest snapshot

--- interactive-computational-graph/src/App.test.tsx
+++ interactive-computational-graph/src/App.test.tsx
@@ -3,6 +3,12 @@ import React from 'react'
 import App from './App'
 
 import { render, screen } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+
+test('renders the app', () => {
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
 
 test('renders title', () => {
   render(<App />)

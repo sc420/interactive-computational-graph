b24dcd8144eb6cfc9b21a264d6fb9ef0118d13f3
Shawn Chang <ebola777gm@gmail.com>
2023-08-06 21:56:50 +0800

    make tests pass

--- interactive-computational-graph/src/App.test.tsx
+++ interactive-computational-graph/src/App.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import renderer from "react-test-renderer";
+import ResizeObserver from "resize-observer-polyfill";
 import App from "./App";
 
 test("renders the app", () => {
@@ -8,6 +9,10 @@ test("renders the app", () => {
 });
 
 test("renders title", () => {
+  // mocks ResizeObserver
+  // Reference: https://github.com/ZeeCoder/use-resize-observer/issues/40#issuecomment-1540994404
+  global.ResizeObserver = ResizeObserver;
+
   render(<App />);
   const linkElement = screen.getByText(/Interactive Computational Graph/i);
   expect(linkElement).toBeInTheDocument();

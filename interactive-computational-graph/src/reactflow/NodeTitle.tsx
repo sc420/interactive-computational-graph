dd01085e07b2ff664b387efda5f8433bf7045d6e
Shawn Chang <ebola777gm@gmail.com>
2023-08-12 20:13:19 +0800

    separate node title

---
+++ interactive-computational-graph/src/reactflow/NodeTitle.tsx
@@ -0,0 +1,41 @@
+import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
+import EditIcon from "@mui/icons-material/Edit";
+import { Box, Grid, IconButton, Typography } from "@mui/material";
+import { type FunctionComponent } from "react";
+
+interface NodeTitleProps {
+  graphId: string;
+  backgroundColor: string;
+}
+
+const NodeTitle: FunctionComponent<NodeTitleProps> = ({
+  graphId,
+  backgroundColor,
+}) => {
+  return (
+    <Box
+      borderBottom={1}
+      borderColor="divider"
+      // corresponds to dragHandle when creating new reactflow.Node
+      className="drag-handle"
+      sx={{ backgroundColor }}
+      px={0.5}
+    >
+      <Grid alignItems="center" container justifyContent="space-between">
+        <Grid item>
+          <Box display="flex" alignItems="center">
+            <DragIndicatorIcon fontSize="small" />
+            <Typography fontWeight={500}>{graphId}</Typography>
+          </Box>
+        </Grid>
+        <Grid item>
+          <IconButton size="small">
+            <EditIcon fontSize="small" />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default NodeTitle;
--- interactive-computational-graph/src/reactflow/OperationNode.tsx
+++ interactive-computational-graph/src/reactflow/OperationNode.tsx
@@ -1,64 +1,21 @@
-import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
-import EditIcon from "@mui/icons-material/Edit";
-import {
-  Box,
-  Grid,
-  IconButton,
-  Stack,
-  Typography,
-  useTheme,
-} from "@mui/material";
-import { blue } from "@mui/material/colors";
+import { Box, Stack, useTheme } from "@mui/material";
 import { type FunctionComponent } from "react";
 import { type NodeProps } from "reactflow";
 import type NodeData from "../features/NodeData";
 import InputItems from "./InputItems";
+import NodeTitle from "./NodeTitle";
 import OutputHandle from "./OutputHandle";
+import { blue } from "@mui/material/colors";
 
 interface OperationNodeProps extends NodeProps {
   data: NodeData;
 }
 
 const handleSize = 20;
-const idFontSize = 14;
 const contentPadding = 1;
 
 const OperationNode: FunctionComponent<OperationNodeProps> = ({ data }) => {
   const theme = useTheme();
-
-  // const getHeight = useCallback((numInputPorts: number): number => {
-  //   // height is divided by (numInputPorts + 1) gaps
-  //   const inputPortHeight = (data.inputPorts.length + 1) * inputPortMargin;
-  //   return Math.max(minNodeHeight, inputPortHeight);
-  // }, []);
-
-  // const getHandleTop = useCallback(
-  //   (index: number, numInputPorts: number): string => {
-  //     // height is divided by (numInputPorts + 1) gaps
-  //     // port is located at the bottom of the index-th gap
-  //     const percentage = ((index + 1) / (numInputPorts + 1)) * 100;
-  //     return `${percentage}%`;
-  //   },
-  //   [],
-  // );
-
-  // const getHandleWidth = useCallback((portName: string): number => {
-  //   return 16 + measureTextWidth(portName);
-  // }, []);
-
-  // const measureTextWidth = useCallback((text: string): number => {
-  //   const dummyElement = document.createElement("div");
-  //   dummyElement.style.visibility = "hidden";
-  //   dummyElement.style.position = "absolute";
-  //   dummyElement.style.whiteSpace = "nowrap";
-  //   dummyElement.innerText = text;
-  //   document.body.appendChild(dummyElement);
-
-  //   const width = dummyElement.getBoundingClientRect().width;
-  //   document.body.removeChild(dummyElement);
-
-  //   return width;
-  // }, []);
 
   return (
     <>
@@ -71,32 +28,7 @@ const OperationNode: FunctionComponent<OperationNodeProps> = ({ data }) => {
       >
         <Stack>
           {/* Title */}
-          <Box
-            borderBottom={1}
-            borderColor="divider"
-            // corresponds to dragHandle when creating new reactflow.Node
-            className="drag-handle"
-            sx={{
-              bgcolor: blue[300],
-            }}
-            px={0.5}
-          >
-            <Grid alignItems="center" container justifyContent="space-between">
-              <Grid item>
-                <Box display="flex" alignItems="center">
-                  <DragIndicatorIcon fontSize="small" />
-                  <Typography fontSize={idFontSize} fontWeight={500}>
-                    {data.graphId}
-                  </Typography>
-                </Box>
-              </Grid>
-              <Grid item>
-                <IconButton size="small">
-                  <EditIcon fontSize="small" />
-                </IconButton>
-              </Grid>
-            </Grid>
-          </Box>
+          <NodeTitle graphId={data.graphId} backgroundColor={blue[300]} />
 
           {/* Content */}
           <Stack sx={{ cursor: "default" }} p={contentPadding} spacing={0.5}>

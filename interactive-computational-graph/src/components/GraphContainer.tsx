45da96ec1236ffadcee30eb6f32d57a97c8a1754
Shawn Chang <ebola777gm@gmail.com>
2023-08-12 23:52:30 +0800

    remove initial nodes

--- interactive-computational-graph/src/components/GraphContainer.tsx
+++ interactive-computational-graph/src/components/GraphContainer.tsx
@@ -31,19 +31,7 @@ const GraphContainer: React.FunctionComponent<GraphContainerProps> = ({
 }) => {
   const [graphStateController] = useState(new GraphStateController());
 
-  const [nodes, setNodes] = useState<Node[]>([
-    {
-      id: "-1",
-      data: { label: "Hello" },
-      position: { x: 0, y: 0 },
-      type: "input",
-    },
-    {
-      id: "-2",
-      data: { label: "World" },
-      position: { x: 100, y: 100 },
-    },
-  ]);
+  const [nodes, setNodes] = useState<Node[]>([]);
   const [edges, setEdges] = useState<Edge[]>([]);
   const [operations, setOperations] = useState<Operation[]>([
     {

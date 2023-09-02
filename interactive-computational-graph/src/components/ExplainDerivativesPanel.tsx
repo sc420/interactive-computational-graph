12f8e8104c37bb637df1a891ae0296b3b8983f4b
Shawn Chang <ebola777gm@gmail.com>
2023-09-03 00:21:26 +0800

    try to use katex

--- interactive-computational-graph/src/components/ExplainDerivativesPanel.tsx
+++ interactive-computational-graph/src/components/ExplainDerivativesPanel.tsx
@@ -1,5 +1,6 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Link, Stack, Typography } from "@mui/material";
 import { type FunctionComponent } from "react";
+import Katex from "../latex/katex";
 
 const ExplainDerivativesPanel: FunctionComponent = () => {
   return (
@@ -16,7 +17,24 @@ const ExplainDerivativesPanel: FunctionComponent = () => {
           <Typography variant="subtitle1">Explain derivatives</Typography>
         </Grid>
       </Grid>
-      ...
+
+      <Stack spacing={1}>
+        <Katex latex="\frac{\partial{e}}{\partial{b}} = \frac{\partial{c}}{\partial{b}} \cdot \frac{\partial{e}}{\partial{c}} + \frac{\partial{d}}{\partial{b}} \cdot \frac{\partial{e}}{\partial{d}}"></Katex>
+        <Katex latex="\displaystyle \frac{\partial{e}}{\partial{b}} = \frac{\partial{c}}{\partial{b}} \cdot \frac{\partial{e}}{\partial{c}} + \frac{\partial{d}}{\partial{b}} \cdot \frac{\partial{e}}{\partial{d}}"></Katex>
+        <Katex latex="\sum_i^k x_i"></Katex>
+        <Katex latex="\displaystyle \sum_i^k x_i"></Katex>
+        <Katex latex="\frac{\partial{e}}{\partial{x_i}}\unknown"></Katex>
+        <span>
+          please click
+          <Link href="#">
+            <Katex latex="\displaystyle \frac{\partial{e}}{\partial{c}}"></Katex>
+          </Link>
+          and
+          <Link href="#">
+            <Katex latex="\displaystyle \frac{\partial{e}}{\partial{d}}"></Katex>
+          </Link>
+        </span>
+      </Stack>
     </>
   );
 };

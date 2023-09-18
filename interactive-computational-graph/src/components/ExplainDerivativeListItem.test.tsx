a5254d244a91e2cb6f8d93e99f56e02d0632dbe7
Shawn Chang <ebola777gm@gmail.com>
2023-09-18 23:56:01 +0800

    add test

---
+++ interactive-computational-graph/src/components/ExplainDerivativeListItem.test.tsx
@@ -0,0 +1,61 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import type ExplainDerivativeItem from "../features/ExplainDerivativeItem";
+import ExplainDerivativesListItem from "./ExplainDerivativeListItem";
+
+test("should trigger event when clicking the copy latex icon", () => {
+  const item: ExplainDerivativeItem = {
+    type: "previousDerivativesReplaced",
+    descriptionParts: [
+      {
+        type: "latexLink",
+        id: "chainRuleTerm-v1",
+        latex: "x",
+        nodeId: "v1",
+      },
+    ],
+    latex: "123",
+  };
+  const handleCopyLatex = jest.fn();
+  const handleClickLatexLink = jest.fn();
+  render(
+    <ExplainDerivativesListItem
+      item={item}
+      hasDivider={false}
+      onCopyLatex={handleCopyLatex}
+      onClickLatexLink={handleClickLatexLink}
+    />,
+  );
+
+  const copyIcon = screen.getByRole("button", { name: "copy" });
+  fireEvent.click(copyIcon);
+  expect(handleCopyLatex).toHaveBeenCalledWith("123");
+});
+
+test("should trigger event when clicking the latex link", () => {
+  const item: ExplainDerivativeItem = {
+    type: "previousDerivativesReplaced",
+    descriptionParts: [
+      {
+        type: "latexLink",
+        id: "chainRuleTerm-v1",
+        latex: "x",
+        nodeId: "v1",
+      },
+    ],
+    latex: "123",
+  };
+  const handleCopyLatex = jest.fn();
+  const handleClickLatexLink = jest.fn();
+  render(
+    <ExplainDerivativesListItem
+      item={item}
+      hasDivider={false}
+      onCopyLatex={handleCopyLatex}
+      onClickLatexLink={handleClickLatexLink}
+    />,
+  );
+
+  const link = screen.getByRole("link");
+  fireEvent.click(link);
+  expect(handleClickLatexLink).toHaveBeenCalledWith("v1");
+});

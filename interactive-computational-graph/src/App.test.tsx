23b509528400075781234e7ecf3d4cc301ac0bbd
Shawn Chang <ebola777gm@gmail.com>
2023-10-01 19:45:46 +0800

    remove snapshot testing

--- interactive-computational-graph/src/App.test.tsx
+++ interactive-computational-graph/src/App.test.tsx
@@ -1,13 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { mockReactFlow } from "./ReactFlowMock";
-// import renderer from "react-test-renderer";
-
-// TODO(sc420): Uncomment when UI becomes stable
-// test("renders the app", () => {
-//   const tree = renderer.create(<App />).toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
 
 beforeAll(() => {
   mockReactFlow();

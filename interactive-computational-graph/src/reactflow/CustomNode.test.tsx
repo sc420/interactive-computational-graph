b78af76ba9e9890b6f86efa9d713826c3db6091a
Shawn Chang <ebola777gm@gmail.com>
2024-01-04 00:25:01 +0800

    add tests

---
+++ interactive-computational-graph/src/reactflow/CustomNode.test.tsx
@@ -0,0 +1,87 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { mockReactFlow } from "../ReactFlowMock";
+import type NodeData from "../features/NodeData";
+import CustomNode from "./CustomNode";
+
+jest.mock("../latex/Katex");
+
+jest.mock("reactflow", () => ({
+  ...jest.requireActual("reactflow"),
+  Handle: () => <div />,
+}));
+
+beforeAll(() => {
+  mockReactFlow();
+});
+
+test("should render the custom node", () => {
+  const data = getFakeData();
+  render(
+    <CustomNode
+      id="0"
+      data={data}
+      selected={false}
+      type=""
+      zIndex={0}
+      isConnectable={true}
+      xPos={0}
+      yPos={0}
+      dragging={false}
+    />,
+  );
+
+  expect(screen.getByText("a")).toBeInTheDocument();
+  expect(screen.getByText("=")).toBeInTheDocument();
+});
+
+test("should trigger event when clicking the node body", () => {
+  const data = getFakeData();
+  const handleBodyClick = jest.fn();
+  data.onBodyClick = handleBodyClick;
+  render(
+    <CustomNode
+      id="0"
+      data={data}
+      selected={false}
+      type=""
+      zIndex={0}
+      isConnectable={true}
+      xPos={0}
+      yPos={0}
+      dragging={false}
+    />,
+  );
+
+  const portLabel = screen.getByText("a");
+  fireEvent.click(portLabel);
+  expect(handleBodyClick).toBeCalledWith("0");
+});
+
+const getFakeData = (): NodeData => {
+  return {
+    name: "a_1",
+    featureNodeType: { nodeType: "OPERATION", operationId: "op1" },
+    inputItems: [
+      {
+        id: "a",
+        label: "a",
+        showHandle: true,
+        showInputField: true,
+        value: "0",
+      },
+    ],
+    outputItems: [
+      {
+        type: "VALUE",
+        labelParts: [{ type: "text", id: "equal", text: "=" }],
+        value: "0",
+      },
+    ],
+    onNameChange: jest.fn(),
+    onInputChange: jest.fn(),
+    onBodyClick: jest.fn(),
+    onDerivativeClick: jest.fn(),
+    isDarkMode: false,
+    isHighlighted: false,
+  };
+};
---
+++ interactive-computational-graph/src/reactflow/InputItems.test.tsx
@@ -0,0 +1,59 @@
+import { fireEvent, render, screen, within } from "@testing-library/react";
+import type NodeData from "../features/NodeData";
+import InputItems from "./InputItems";
+
+jest.mock("../latex/Katex");
+
+jest.mock("reactflow", () => ({
+  ...jest.requireActual("reactflow"),
+  Handle: () => <div />,
+}));
+
+test("should trigger event when changing the input value", () => {
+  const data: NodeData = {
+    name: "a_1",
+    featureNodeType: { nodeType: "OPERATION", operationId: "op1" },
+    inputItems: [
+      {
+        id: "a",
+        label: "a",
+        showHandle: true,
+        showInputField: true,
+        value: "0",
+      },
+    ],
+    outputItems: [
+      {
+        type: "VALUE",
+        labelParts: [{ type: "text", id: "equal", text: "=" }],
+        value: "0",
+      },
+    ],
+    onNameChange: jest.fn(),
+    onInputChange: jest.fn(),
+    onBodyClick: jest.fn(),
+    onDerivativeClick: jest.fn(),
+    isDarkMode: false,
+    isHighlighted: false,
+  };
+  const handleInputChange = jest.fn();
+  render(
+    <InputItems
+      id="0"
+      data={data}
+      itemHeight={10}
+      inputWidth={10}
+      handleLeftOffset={"-0px"}
+      handleSize={10}
+      handleColor={"black"}
+      handleHoverColor={"black"}
+      handleBorderColor={"black"}
+      onInputChange={handleInputChange}
+    />,
+  );
+
+  const inputField = screen.getByTestId("input-item-0-a");
+  const input = within(inputField).getByRole("textbox");
+  fireEvent.change(input, { target: { value: "3" } });
+  expect(handleInputChange).toBeCalledWith("a", "3");
+});

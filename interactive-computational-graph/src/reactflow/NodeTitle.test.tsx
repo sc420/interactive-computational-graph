548504294df3d4878b81b67f55bea49e62f0ae5b
Shawn Chang <ebola777gm@gmail.com>
2024-01-21 14:47:42 +0800

    make node not draggable when editing the name

--- interactive-computational-graph/src/reactflow/NodeTitle.test.tsx
+++ interactive-computational-graph/src/reactflow/NodeTitle.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import type OperationNodeData from "../features/OperationNodeData";
 import NodeTitle from "./NodeTitle";
 
@@ -20,6 +20,36 @@ test("should render the name", () => {
   );
 
   expect(screen.getByText("abc")).toBeInTheDocument();
+});
+
+test("should not have draggable class when editing the name", () => {
+  const operationData = getOperationData();
+  const handleNameChange = jest.fn();
+  render(
+    <NodeTitle
+      id="1"
+      name="abc"
+      operationData={operationData}
+      backgroundColor="white"
+      isDarkMode={false}
+      isHighlighted={false}
+      onNameChange={handleNameChange}
+    />,
+  );
+
+  const nodeTitle = screen.getByTestId("node-title-1");
+  const editIcon = screen.getByLabelText("edit");
+
+  expect(nodeTitle).toHaveClass("drag-handle");
+
+  fireEvent.click(editIcon);
+
+  expect(nodeTitle).not.toHaveClass("drag-handle");
+
+  const editingInput = screen.getByLabelText("editingName");
+  fireEvent.blur(editingInput);
+
+  expect(nodeTitle).toHaveClass("drag-handle");
 });
 
 test("should not have striped animation when not highlighted", () => {
--- interactive-computational-graph/src/reactflow/NodeTitle.tsx
+++ interactive-computational-graph/src/reactflow/NodeTitle.tsx
@@ -1,6 +1,6 @@
 import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
 import { Box, Stack } from "@mui/material";
-import { useMemo, type FunctionComponent } from "react";
+import { useCallback, useMemo, useState, type FunctionComponent } from "react";
 import type OperationNodeData from "../features/OperationNodeData";
 import EditableName from "./EditableName";
 import "./NodeTitle.css";
@@ -31,11 +31,25 @@ const NodeTitle: FunctionComponent<NodeTitleProps> = ({
     return isDarkMode ? "striped-animation-dark" : "striped-animation-light";
   }, [isDarkMode, isHighlighted]);
 
+  const [isEditingName, setIsEditingName] = useState(false);
+
+  const getClassName = useCallback(() => {
+    if (isEditingName) {
+      return `${animationClassName}`;
+    } else {
+      return `drag-handle ${animationClassName}`;
+    }
+  }, [animationClassName, isEditingName]);
+
+  const handleEditingChange = useCallback((isEditing: boolean) => {
+    setIsEditingName(isEditing);
+  }, []);
+
   return (
     <Box
       data-testid={`node-title-${id}`}
       // corresponds to dragHandle when creating new reactflow.Node
-      className={`drag-handle ${animationClassName}`}
+      className={getClassName()}
       display="flex"
       sx={{ backgroundColor }}
       p={0.5}
@@ -48,6 +62,7 @@ const NodeTitle: FunctionComponent<NodeTitleProps> = ({
           <EditableName
             name={name}
             operationData={operationData}
+            onEditingChange={handleEditingChange}
             onNameChange={onNameChange}
           />
         </Box>

9448ecc085b94955509865db2c4522824eaf5975
Shawn Chang <ebola777gm@gmail.com>
2024-04-07 04:37:53 +0800

    make handles bigger

--- interactive-computational-graph/src/reactflow/styles.ts
+++ interactive-computational-graph/src/reactflow/styles.ts
@@ -1,6 +1,6 @@
 const itemHeight = 40;
 const inputWidth = 120;
-const handleSize = 20;
+const handleSize = 25;
 const bodyPadding = 1;
 
 export { itemHeight, inputWidth, handleSize, bodyPadding };

bae555e96c85364e271088b4c5ffd66cf242a9e8
Shawn Chang <ebola777gm@gmail.com>
2024-04-06 18:47:00 +0800

    add more examples

--- interactive-computational-graph/src/components/WelcomeDialog.tsx
+++ interactive-computational-graph/src/components/WelcomeDialog.tsx
@@ -21,7 +21,15 @@ import {
   useState,
   type FunctionComponent,
 } from "react";
+import advancedArithmetic1 from "../examples/advanced_arithmetic1.json";
+import advancedArithmetic2 from "../examples/advanced_arithmetic2.json";
+import advancedArithmetic3 from "../examples/advanced_arithmetic3.json";
 import basicArithmetic1 from "../examples/basic_arithmetic1.json";
+import basicArithmetic2 from "../examples/basic_arithmetic2.json";
+import basicArithmetic3 from "../examples/basic_arithmetic3.json";
+import neuralNetwork1 from "../examples/neural_network1.json";
+import neuralNetwork2 from "../examples/neural_network2.json";
+import neuralNetwork3 from "../examples/neural_network3.json";
 import type GraphContainerState from "../states/GraphContainerState";
 
 interface ExampleItem {
@@ -39,24 +47,24 @@ const exampleSections: ExampleSection[] = [
     title: "Basic Arithmetic",
     exampleItems: [
       { latex: "f = x + y", state: basicArithmetic1 as GraphContainerState },
-      { latex: "f = x * y", state: basicArithmetic1 as GraphContainerState },
-      { latex: "f = a * x^n", state: basicArithmetic1 as GraphContainerState },
+      { latex: "f = x * y", state: basicArithmetic2 as GraphContainerState },
+      { latex: "f = a * x^n", state: basicArithmetic3 as GraphContainerState },
     ],
   },
   {
     title: "Advanced Arithmetic",
     exampleItems: [
       {
-        latex: "f = (a+b)*(c+d)",
-        state: basicArithmetic1 as GraphContainerState,
-      },
-      {
         latex: "f = (a+b)^(c+d)",
-        state: basicArithmetic1 as GraphContainerState,
+        state: advancedArithmetic1 as GraphContainerState,
       },
       {
-        latex: "f = (sin(x) + cos(y)) / (sin(x) * cos(y))",
-        state: basicArithmetic1 as GraphContainerState,
+        latex: "f = ln(x^2) + ln(y^2)",
+        state: advancedArithmetic2 as GraphContainerState,
+      },
+      {
+        latex: "f = sin(x) / x",
+        state: advancedArithmetic3 as GraphContainerState,
       },
     ],
   },
@@ -65,15 +73,15 @@ const exampleSections: ExampleSection[] = [
     exampleItems: [
       {
         latex: "2-1 fully connected",
-        state: basicArithmetic1 as GraphContainerState,
+        state: neuralNetwork1 as GraphContainerState,
       },
       {
         latex: "2-2-1 fully connected",
-        state: basicArithmetic1 as GraphContainerState,
+        state: neuralNetwork2 as GraphContainerState,
       },
       {
-        latex: "2-3-4-1 fully connected",
-        state: basicArithmetic1 as GraphContainerState,
+        latex: "5-4-3-2 fully connected",
+        state: neuralNetwork3 as GraphContainerState,
       },
     ],
   },
@@ -100,6 +108,7 @@ const WelcomeDialog: FunctionComponent<WelcomeDialogProps> = ({ onLoad }) => {
       open={open}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
+      maxWidth="md"
     >
       <DialogTitle id="alert-dialog-title">Get Started</DialogTitle>
       <DialogContent>
